591add36ac5082a1881d3dddfd0e8bc659809193
Thomas Anderson <ahmetacar1111@gmail.com>
2022-06-29 03:36:00 -0400

    Work on new practices

--- Objects/javascript.js
+++ Objects/javascript.js
@@ -14,47 +14,12 @@ let user2 = {     // An object
     name: "John", // The key 'name' store value 'john'
     age: 30       // The key 'age' store value 30
 };
-
-console.log(user2.name); // John
-console.log(user2.age);  // 30
-
-"REMOVE A PROPERTY"
-
-// To remove a property we use the delete operator.
-delete user2.age;
-console.log(user2); // {name:'John'}
-
-// We can also use multiword property names, but then they must be quoted,
-let user3 = {
-    name: 'John',
-    age: 30,
-    'likes this': true, // Multiword property must be quoted
-};
-
-// The last propery in the list may end with a comma:
-//  This is called a 'trailing' or 'hanging' comma. 
-//    Makes it easier to add/remove/move around properties, because all lines become alike.
-
-// For multiword properties, the dot access doesn't work.
-// user3.likes birds = true; // Syntax Error!
-
-let user4 = {};
-
-// Set
-user4["likes birds"] = true;
-
-// Get
-// console.log(user['likes birds']); // True
-
-// Delete
-delete user4['likes birds'];
-
-// console.log(user4); // {}
+Infinity
 
 let key = 'likes birds';
 
 user4[key] = true;
-console.log(user4);
+// console.log(user4);
 
 
 // Here, the variable 'key' may be calculated at run-time or depend on the user input. 
@@ -94,7 +59,7 @@ let bag1 = {};
 
 // Take property name from the fruit variable.
 // bag1[fruit] = 5;
-console.log(bag1); // {<fruit>: 5};
+// console.log(bag1); // {<fruit>: 5};
 
 // We can use more complex expressions inside square brackets.
 
@@ -102,7 +67,7 @@ let fruit1 = 'apple';
 let bag2 = {
     [fruit1 + 'Computers']: 5 // bag.appleComputers = 5
 }
-console.log(bag2);
+// console.log(bag2);
 
 /*
     So most of the time, when property names are known and simple, the dot is used. And if 
@@ -124,7 +89,7 @@ function makeUser(name,age) {
 }
 
 let user6 = makeUser("John",40);
-console.log(user6.age);
+// console.log(user6.age);
 
 /*
     In the example above, properties have the same names as variables. The use-case of 
@@ -172,8 +137,8 @@ let obj2 = {
     0: 'test' // same as '0':"test"
 }
 
-console.log(obj2['0']); // both returns test
-console.log(obj2[0]);   // both returns test
+// console.log(obj2['0']); // both returns test
+// console.log(obj2[0]);   // both returns test
 
 /*
     There's a minor gotcha with a special property name __proto__. we can't set it to a 
@@ -182,11 +147,11 @@ console.log(obj2[0]);   // both returns test
 
 let obj3 = {};
 obj3.__proto__ =5; // assign a number
-console.log(obj3.__proto__); // Didn't work as intended
+// console.log(obj3.__proto__); // Didn't work as intended
 
 
 let user8 = {};
-console.log(user8.noSuchProperty === undefined); // true means 'no such property'
+// console.log(user8.noSuchProperty === undefined); // true means 'no such property'
 
 
 // There's also a special operator 'in' for that.
@@ -195,8 +160,8 @@ console.log(user8.noSuchProperty === undefined); // true means 'no such property
 // EX:
 
 let user9 = {name: 'Jonathan', age: 35};
-console.log('age' in user9 ); // True, user.age exist
-console.log('asdasd' in user9); // false, user.asdasd. doesn't exist
+// console.log('age' in user9 ); // True, user.age exist
+// console.log('asdasd' in user9); // false, user.asdasd. doesn't exist
 
 
 /*
@@ -209,7 +174,7 @@ console.log('asdasd' in user9); // false, user.asdasd. doesn't exist
 
 let user0 = {age:30};
 let key2 = 'age';
-console.log(key2 in user0); // true, propery 'age' exists
+// console.log(key2 in user0); // true, propery 'age' exists
 
 
 /*
@@ -228,8 +193,8 @@ let obj4 ={
 
 
 
-console.log(obj4.test); // it's undefined, so - no such property???
-console.log('test' in obj4); // true!! the property exist!
+// console.log(obj4.test); // it's undefined, so - no such property???
+// console.log('test' in obj4); // true!! the property exist!
 
 "THE 'for...in' loop! "
 
@@ -254,9 +219,9 @@ let user11 = {
 
 for (let key in user11) {
     // keys
-    console.log(key); // name, age, isAdmin
+    // console.log(key); // name, age, isAdmin
     // values for the keys
-    console.log(user11[key]); // John, 30, true
+    // console.log(user11[key]); // John, 30, true
 }
 
 "ORDERED LIKE AN OBJECT:"
@@ -280,7 +245,7 @@ let codes = {
 };
 
 for (let code in codes) {
-    console.log(code);
+    // console.log(code);
 }
 /*
      The object may be used to suggest a list of options to the user. If we're making a 
@@ -304,7 +269,7 @@ let user12 = {
 user12.age = 25; // add one more
 
 for (let prop in user12) {
-    console.log(prop); //name, lastName,age
+    // console.log(prop); //name, lastName,age
 }
 
 /*          SUMMARY
@@ -338,7 +303,7 @@ function isEmpty(obj) {
     for (let key in obj) return false; return true;
 }
 let schedule = {};
-console.log(isEmpty(schedule));
+// console.log(isEmpty(schedule));
 
 
 let salaries = {
@@ -351,7 +316,7 @@ let sum = 0;
 for (let key in salaries) {
     sum += salaries[key];
 }
-console.log(sum);
+// console.log(sum);
 
 let menu = {
     widht:200,
@@ -368,7 +333,7 @@ function multiplyNumeric(obj) {
 }
 
 multiplyNumeric(menu);
-console.log(menu);
+// console.log(menu);
 
 /*
     Let's see how MND tutorial goes with objects!
@@ -411,25 +376,25 @@ const person = {
 "Bracket Notation: "
 
 // instead of person.name.first we can use:
-console.log(person['name']['first']); // Bob!
+// console.log(person['name']['first']); // Bob!
 
 
 "Setting Object Members: "
 person.age = 45;
 person.name.last = 'Acar'
-console.log(person.name.last);
+// console.log(person.name.last);
 person['name'].last = 'Turk';
-console.log(person.name.last);
+// console.log(person.name.last);
 person.name['first'] = 'Seda';
-console.log(person['name'].first); // we can use different combinations together!
+// console.log(person['name'].first); // we can use different combinations together!
 
 // can also change or create new functions:
 person['eyes'] = 'hazel';
 person.farewell = function() {
     console.log('Bye Everybody!');
 }
-console.log('eyes:' + person['eyes']);
-console.log(person.farewell());
+// console.log('eyes:' + person['eyes']);
+// console.log(person.farewell());
 
 // We can also add member names using bracket notation 
 
@@ -458,14 +423,14 @@ person[myDataName] = myDataValue;
 const person1 = {
     name:'Chris',
     introduceSelf() {
-        console.log(`Hi! I'm ${this.name}.`)
+        // console.log(`Hi! I'm ${this.name}.`)
     }
 }
 
 const person2 = {
     name:'Deepti',
     introduceSelf() {
-        console.log(`Hi! I'm ${this.name}.`)
+        // console.log(`Hi! I'm ${this.name}.`)
     }
 }
 
@@ -478,6 +443,268 @@ const person2 = {
     than one object fomr a single object definition, and that's the subject of 
     the next section!
 */
+
+
+
+"INTRODUCING CONSTRUCTORS: "
+
+/*  
+    Using object literals is fine when you only need to create one object,
+    but if you have to create more than one, as in the previous section,
+    they're seriously inadequate. We have to write out the same code for every 
+    object we create, and if we want to change some properties of the object -
+    like adding a height property - then we have to remember to update every 
+    object!
+    
+    We would like a define the 'shape' of an object -- the set of methods and the 
+    properties it can have -- and then create as many objects as we like, just updating 
+    the values for the properties that are different.
+
+    The first version of this just a function:
+*/
+
+
+
+
+function createPerson(name) {
+    const obj = {};
+    obj.name = name;
+    obj.introduceSelf = function() {
+        console.log(`Hi! I'm ${this.name}.`)
+    }
+    return obj;
+}
+ahmetObj = createPerson('ahmet');
+// console.log(ahmetObj.introduceSelf());
+/*
+    This function creates and returns a new object each time we call it.
+    The object will have two members:
+    - A property name
+    - A method called introduceSelf()
+
+    Note that createPerson() takes a parameter name to set the value of the name
+    property, but the value of the introduceSelf() method will be the same for all
+    objects created using this function. This is a very common pattern for creating 
+    objects.
+
+    Now we can create as many objects as we like, reusing the definition.
+*/
+
+
+const selva = createPerson('Selva');
+// console.log(selva.name);
+// console.log(selva.introduceSelf());
+
+const frankie = createPerson('Frankie');
+// console.log(frankie.name);
+// console.log(frankie.introduceSelf());
+
+
+/*
+    This works fine but is a bit long-winded: we have to create an 
+    empty object, initialize it, and return it. A better way is to use a 'constructor'.
+    A constructor is just a function called using the new keyword. When you call a
+    constructor, it will:
+     - Create a new object,
+     - Bind this to the new object, so you can refer to this in your constructor code.
+     - run the code in the constructor
+     - return the new object.
+
+    Contructors, by convention, start with a capital letter and are named for the type
+    of object they create. So we could rewrite our example like this:
+*/
+
+
+
+function Person(name) { // no initializer like const obj
+    this.name = name;
+    this.introduceSelf = function() {
+        // console.log(`Hi! I'm ${this.name}.`);
+    }
+}                       // no returning like return obj
+
+const salva = new Person('Salva'); // To call Person() as a constructor, we use new:
+// console.log(salva.name);
+
+/* You've been using objects all along!!!:
+    As you've been going through these examples, you have probably been thinking that
+    the dot notation you've been using is very familiar. That's because you've been 
+    using it throughout the course! Every time we've been working through an example 
+    taht uses a built-in API or JS object, we've been using objects, because such
+    features are built using exactly the same kind of object structures that we've 
+    been looking at here, albeit(although) more complex ones than in our own basic
+    custom examples.
+
+    So when you used string methods like:
+
+    myString.split(',');
+
+    You were using a method available on a String object. Every time you create a string
+    in your code, that string is automatically created as an instance of String, and therefore
+    has several common methods and properties available on it.
+
+    When you accessed the document object model using lines like this:
+
+    const myDiv = document.createElement('div');
+    const myVideo = document.querySelector('video');
+
+    You were using methods available on 'Document' object. For each webpage loaded,
+    an instance of 'Document' is created, called document, which represents the entire
+    page's structure, content, and other featurs as its URL. Again, this means that it 
+    has several common methods and properties available on it.
+
+    The same is true of pretty much any other built-in object or API you've been using
+    --Array,Math, and so on.
+
+    Note that built in objects and APIs don't always create object instances automatically.
+    As an example, the Notifications API --- which allows modern browsers to fire system
+    notifications -- requires you to instantiate a new object instance using the constructor
+    for each notification you want to fire. Try entering the following into your JS console.
+*/
+
+myNotification = new Notification('Hello!');
+
+// Object examples:
+
+const seksendort = {
+    name:'SeksenDort',
+    nationality: 'Turkish',
+    genre:'Rock',
+    members:['Tuna Velibasoglu','Umitcan Tuncer','Serter Karadeniz','Ismail Okan Ozen'],
+    formed:2005,
+    split:false,
+    albums: [
+        {name:'Akiyor Zaman',
+        released:2011},
+        {name:'Ruya',
+        released:2012},],
+}
+
+let bandInfo = `The ${seksendort.nationality} ${seksendort.genre} Band ${seksendort.name} has
+    founded by ${seksendort.members} in ${seksendort.formed}. Their first album,${seksendort.albums[0].name}
+    was released in ${seksendort.albums[0].released}.`
+console.log(bandInfo);    
+
+
+function Cat(name,color,breed) {
+    this.name = name;
+    this.color = color;
+    this.breed = breed;
+    this.greeting = function() {
+        console.log(`Hello,said ${this.name} the ${this.breed}`);
+    }
+}
+
+const cat1 = new Cat('Bertie','white','Cymric');
+const cat2 = new Cat('Elfie','ginger','Aphrodite Giant');
+
+
+/*-----------------------------------------------------------------------*/
+"JS Function Parameters and Arguments: "
+
+// Function parameters are the names listed in the function definition,
+// Function arguments are the real values passed to (and received by) the function.
+
+/* Parameter Rules:
+    - JS function definitions do not specify data types for parameters.
+    - JS functions do not perform type checking on the passed arguments.
+    - JS functions do not check the number of arguments received.
+*/
+
+'Default Parameters: '
+
+/* If a function is called with missing arguments (less than declared), the missing
+    values are set to undefined.
+
+   Sometimes this is acceptable, but sometimes it is better to assign a default value to 
+    the parameter.
+*/
+
+/* As an example:
+    function myFunction(x,y) {
+        if (y === undefined) {
+            y = 2;
+        }
+    }
+
+    Instead of the above, we can use:
+
+    function myFunction(x,y = 2) {
+        // function code
+    }
+*/
+
+
+'The Arguments Object: '
+
+/*
+    JS functions have a built-in object called the arguments object.
+    The argument object contains an array of the arguments used when the 
+      function was called (invoked).
+    This way you can simply use a function to find (for instance) the highest 
+      value in a list of numbers: EX:
+*/
+
+let x = findMax(1,123,333,115,33,55);
+
+function findMax() {
+    let max = -Infinity;
+    for (let i = 0; i < arguments.length; i++) {
+        if(arguments[i] > max) {
+            max = arguments[i];
+        }
+    }
+    return max;
+}
+// console.log(x);
+
+
+x = sumAll(1,123,500,115,44,88);
+
+function sumAll() {
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
+    }
+    return sum;
+}
+// If a function is called with too many arguments (more than declared), 
+//  these arguments can be reached using the arguments object.
+// console.log(x);
+
+
+"ARGUMENTS ARE PASSED BY VALUE: "
+/*  
+    The parameters, in a function call, are the function's arguments.
+    JS arguments are passed by value: The function only gets to know the values, not the
+    argument's locations.
+
+    If a function changes an argument's value, it does not change the parameter's original
+    value.
+
+    Changes to arguments are not visible (reflected) outside function
+*/
+
+
+"OBJECTS ARE PASSED BY REFERENCE: "
+/*
+    In JS, object references are values.
+    Because of this, objects will behave like they are passed by reference:
+    
+    If a function changes an object property, it changes the original value.
+
+    Changes to objects properties are visible (reflected) outside the function.
+*/
+
+function myFunction(firstN,lastN) {
+    this.firstName = firstN;
+    this.lastName = lastN;
+    this.sayHello = function() {
+        console.log(`Hello! My name is ${this.firstName} ${this.lastName}`);
+    };
+}
+ahmetObject = new myFunction('Ahmed','Acar');
+console.log(ahmetObject.sayHello());
 
 
 
