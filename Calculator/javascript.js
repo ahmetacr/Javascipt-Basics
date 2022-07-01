92974083bf22d3811748ba8fdf3913697c75c12c
Thomas Anderson <ahmetacar1111@gmail.com>
2022-06-30 23:21:52 -0400

    Calculator works but there are still some possible errors

--- Calculator/javascript.js
+++ Calculator/javascript.js
@@ -1,50 +1,86 @@
 // WELCOME TO THE CALCULATOR PROJECT!
-// Lets first get our variables
-// First the operators!
-const divideBtn = document.getElementById('divide');
-const multiplyBtn = document.getElementById('multiply');
-const subtractBtn = document.getElementById('subtract');
-const addBtn = document.getElementById('add');
-// And then the numbers:
-const sevenBtn = document.getElementById('seven');
-const eightBtn = document.getElementById('eight');
-const nineBtn = document.getElementById('nine');
-const fourBtn = document.getElementById('four');
-const fiveBtn = document.getElementById('five');
-const sixBtn = document.getElementById('six');
-const oneBtn = document.getElementById('one');
-const twoBtn = document.getElementById('two');
-const threeBtn = document.getElementById('three');
-const zeroBtn = document.getElementById('zero');
-// And the equal sign
+// Lets get the variables
 const equalsBtn = document.getElementById('equals');
-// And the clear Button
 const clearBtn = document.getElementById('clear');
-// And the display box
-const displayBox = document.getElementById('resultP');
-
-
+const displayBox = document.getElementById('processP');
+const resultBox = document.getElementById('result');
+const buttons = document.querySelectorAll('button');
 
 // Define the basic functions
-function add(num1,num2) {return num1 + num2};
-function subtract(num1,num2) {return num1 - num2};
-function multiply(num1,num2) {return num1 * num2};
-function divide(num1,num2) {return num1 / num2};
+function add(num1,num2) {return parseInt(num1) + parseInt(num2)};
+function subtract(num1,num2) {return parseInt(num1) - parseInt(num2)};
+function multiply(num1,num2) {return parseInt(num1) * parseInt(num2)};
+function divide(num1,num2) {return parseInt(num1) / parseInt(num2)};
 
 "Create a new function 'operate' that takes an operator\
     and 2 numbers and then calls one of the above functions on the numbers."
-function operate(operator,num1,num2) {
-    operator(num1,num2);
-}
-
-"Create the functions that populate the \
-  display when you click the number buttons \
-   you should be storing the 'display value' \
-    in a variable somewhere for use in the next step."
+function operate(operator,num1,num2) { return operator(num1,num2); }
 
 let displayValue;
-buttons = document.querySelectorAll('button');
-buttons.forEach(button => button.addEventListener('click',() => {
-    displayValue = button.textContent;
-    displayBox.textContent = displayValue;
-}))
+let number1;
+let number2;
+let operator;
+let emptyResult = false;
+
+function getNum1() {
+    if(this.id != 'divide' && this.id != 'add' && this.id != 'multiply' && this.id != 'add' && this.id != 'subtract' && this.id != 'equals') {
+        displayValue = this.textContent;
+        resultBox.textContent += displayValue;
+        number1 = resultBox.textContent;
+    } else if (this.id == 'equals') {
+        displayBox.textContent += ' ' + this.id;
+    } else {  
+        operator = this.id;
+        displayBox.textContent += number1 + ' ' + this.textContent + ' ';
+        buttons.forEach(button => button.removeEventListener('click',getNum1));
+        emptyResult = true;
+        buttons.forEach(button => button.addEventListener('click',getNum2));
+    }
+}
+
+buttons.forEach(button => button.addEventListener('click',getNum1));
+let answer;
+function getNum2() {
+    if(this.id != 'divide' && this.id != 'add' && this.id != 'multiply' && this.id != 'add' && this.id != 'subtract' && this.id != 'equals') {
+        // if (displayBox.textContent == '+' || displayBox.textContent == '-' || displayBox.textContent == '÷' || displayBox.textContent == '÷' || displayBox.textContent == 'X') {
+        //     displayBox.textContent = '';
+        // }     
+        if (emptyResult) {
+            resultBox.textContent = '';
+        } 
+        displayValue = this.textContent;
+        resultBox.textContent += displayValue; // 9 a bastiysam 9
+        number2 = resultBox.textContent;
+        emptyResult = false;
+    } else if (this.id == 'equals') {
+        if(operator == 'multiply') {
+            answer = operate(multiply,number1,number2);
+        } else if (operator == 'subtract') {
+            answer = operate(subtract,number1,number2);
+        } else if (operator == 'add') {
+            answer = operate(add,number1,number2);
+        } else if (operator == 'divide') {
+            answer = operate(divide,number1,number2);
+        }
+        displayBox.textContent += ' ' + number2 + ' =';
+        resultBox.textContent = answer;
+    } else {  
+        if(operator == 'multiply') {
+            answer = operate(multiply,number1,number2);
+        } else if (operator == 'subtract') {
+            answer = operate(subtract,number1,number2);
+        } else if (operator == 'add') {
+            answer = operate(add,number1,number2);
+        } else if (operator == 'divide') {
+            answer = operate(divide,number1,number2);
+        }
+        resultBox.textContent = answer;
+        emptyResult = true;
+        number1 = answer;
+        number2 = null;
+        operator = this.id;
+        displayBox.textContent = answer + ' ' + this.textContent;
+        buttons.forEach(button => button.removeEventListener('click',getNum2));
+        buttons.forEach(button => button.addEventListener('click',getNum2));
+    }   
+}
