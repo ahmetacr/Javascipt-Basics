8c35003e08ff5925737663cf35fd548a5bfb850b
Thomas Anderson <ahmetacar1111@gmail.com>
2022-07-01 02:48:25 -0400

    ADD clear button, solve duplicate sign problem

--- Calculator/javascript.js
+++ Calculator/javascript.js
@@ -7,44 +7,52 @@ const resultBox = document.getElementById('result');
 const buttons = document.querySelectorAll('button');
 
 // Define the basic functions
-function add(num1,num2) {return parseInt(num1) + parseInt(num2)};
-function subtract(num1,num2) {return parseInt(num1) - parseInt(num2)};
-function multiply(num1,num2) {return parseInt(num1) * parseInt(num2)};
-function divide(num1,num2) {return parseInt(num1) / parseInt(num2)};
+function add(num1,num2) {return parseFloat(num1) + parseFloat(num2)};
+function subtract(num1,num2) {return parseFloat(num1) - parseFloat(num2)};
+function multiply(num1,num2) {return parseFloat(num1) * parseFloat(num2)};
+function divide(num1,num2) {return parseFloat(num1) / parseFloat(num2)};
 
 "Create a new function 'operate' that takes an operator\
     and 2 numbers and then calls one of the above functions on the numbers."
 function operate(operator,num1,num2) { return operator(num1,num2); }
+buttons.forEach(button => button.addEventListener('click',getNum1));
 
+let answer;
 let displayValue;
 let number1;
 let number2;
 let operator;
 let emptyResult = false;
+let waitForSign = false; // This is to prevent error bc of clicking an operator twice or more
 
 function getNum1() {
-    if(this.id != 'divide' && this.id != 'add' && this.id != 'multiply' && this.id != 'add' && this.id != 'subtract' && this.id != 'equals') {
+    if(this.id != 'divide' && this.id != 'add' && this.id != 'multiply' && this.id != 'add' && this.id != 'subtract' && this.id != 'equals' && this.id != 'clear') {
         displayValue = this.textContent;
         resultBox.textContent += displayValue;
         number1 = resultBox.textContent;
+        waitForSign = true;
     } else if (this.id == 'equals') {
-        displayBox.textContent += ' ' + this.id;
+        return;
+    } else if (this.id == 'clear') {
+        number1 = null;
+        number2 = null;
+        displayBox.textContent = '';
+        resultBox.textContent = '';
     } else {  
-        operator = this.id;
-        displayBox.textContent += number1 + ' ' + this.textContent + ' ';
-        buttons.forEach(button => button.removeEventListener('click',getNum1));
-        emptyResult = true;
-        buttons.forEach(button => button.addEventListener('click',getNum2));
+        if(waitForSign) {
+            operator = this.id;
+            displayBox.textContent += number1 + ' ' + this.textContent + ' ';
+            buttons.forEach(button => button.removeEventListener('click',getNum1));
+            emptyResult = true;
+            waitForSign = false;
+            buttons.forEach(button => button.addEventListener('click',getNum2));
+        }
     }
+    return number1;
 }
 
-buttons.forEach(button => button.addEventListener('click',getNum1));
-let answer;
 function getNum2() {
-    if(this.id != 'divide' && this.id != 'add' && this.id != 'multiply' && this.id != 'add' && this.id != 'subtract' && this.id != 'equals') {
-        // if (displayBox.textContent == '+' || displayBox.textContent == '-' || displayBox.textContent == '÷' || displayBox.textContent == '÷' || displayBox.textContent == 'X') {
-        //     displayBox.textContent = '';
-        // }     
+    if(this.id != 'divide' && this.id != 'add' && this.id != 'multiply' && this.id != 'add' && this.id != 'subtract' && this.id != 'equals' && this.id != 'clear') {
         if (emptyResult) {
             resultBox.textContent = '';
         } 
@@ -64,6 +72,14 @@ function getNum2() {
         }
         displayBox.textContent += ' ' + number2 + ' =';
         resultBox.textContent = answer;
+        emptyResult = true;
+    } else if (this.id == 'clear') {
+        number1 = null;
+        number2 = null;
+        displayBox.textContent = '';
+        resultBox.textContent = '';
+        buttons.forEach(button => button.removeEventListener('click',getNum2));
+        buttons.forEach(button => button.addEventListener('click',getNum1));
     } else {  
         if(operator == 'multiply') {
             answer = operate(multiply,number1,number2);
@@ -74,6 +90,9 @@ function getNum2() {
         } else if (operator == 'divide') {
             answer = operate(divide,number1,number2);
         }
+        if (number2 == undefined) {
+            answer = number1;
+        }         
         resultBox.textContent = answer;
         emptyResult = true;
         number1 = answer;
@@ -81,6 +100,7 @@ function getNum2() {
         operator = this.id;
         displayBox.textContent = answer + ' ' + this.textContent;
         buttons.forEach(button => button.removeEventListener('click',getNum2));
-        buttons.forEach(button => button.addEventListener('click',getNum2));
+        buttons.forEach(button => button.addEventListener('click',getNum2));        
     }   
 }
+
